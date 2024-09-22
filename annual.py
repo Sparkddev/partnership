77ce7dc9c5d7b7385c3b6daf69c5c5f300c1687c
latestbob <edidiongbobson@gmail.com>
2024-09-22 12:58:18 +0100

    created annual.py to add annual salary

---
+++ annual.py
@@ -0,0 +1,19 @@
+
+
+# this is to calculate annual salary
+
+# prompt user input their monthly salary 
+
+# multiply the monlthly salary by 12
+
+# output the annual salary
+
+
+monthly_salary = input("Please enter your monthly salary i.e 4000 ")
+
+monthly_salary = float(monthly_salary)
+
+annual_salary = monthly_salary * 12
+
+print(f"Your annual salary is  {annual_salary}")
+
